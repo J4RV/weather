fcb953731f2896110072a4dbf825bfe4f68004ae
Jose Rojo <jrojo@adaptavist.com>
2018-05-14 13:06:13 +0200

    City input validation

--- src/actions/searchCity.js
+++ src/actions/searchCity.js
@@ -3,7 +3,10 @@ import {SET_CITY, SET_WEATHER} from './actionKeys'
 
 const getCityApiUrl = (city) => `https://api.openweathermap.org/data/2.5/forecast?q=${encodeURI(city)}&appid=${APPID}`
 
+const invalidCity = (city) => (city == null || city.trim() === '')
+
 export default (city) => (dispatch) => {
+  if (invalidCity(city)) return
   dispatch({type: SET_CITY, city: city})
   const url = getCityApiUrl(city)
   window.fetch(url)
